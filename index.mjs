2436bb97490f55ec632e16165c79552a5f4477e2
Luis Cruz Mac <luismario2712@gmail.com>
2023-07-27 17:54:25 -0600

    Se agrega login

--- index.mjs
+++ index.mjs
@@ -1,38 +1,36 @@
 let alumns = [];
+let profesors = [];
 
-function fetchMaterias () {
-    fetch('https://universidad-backend.onrender.com/getMaterias')
-    .then(response => response.json())
-    .then(data => {
-
-         let option = data.materias.map(item => `<option value="${item.materia}">${item.materia}</option>`)
-
-        document.getElementById("selecter-materias")
-        .innerHTML = option
-    })
+function fetchMaterias() {
+  fetch("https://universidad-backend.onrender.com/getMaterias")
+    .then((response) => response.json())
+    .then((data) => {
+      let option = data.materias.map(
+        (item) => `<option value="${item.materia}">${item.materia}</option>`
+      );
+      document.getElementById("selecter-materias").innerHTML = option;
+    });
 }
 
-fetchMaterias()
+fetchMaterias();
 
-fetch('https://universidad-backend.onrender.com/obtenerAlumnos')
-.then(response => response.json())
-.then(data => {
+fetch("https://universidad-backend.onrender.com/obtenerAlumnos")
+  .then((response) => response.json())
+  .then((data) => {
     alumns = data.alumnos;
     console.log(alumns);
-    showAlumnos(alumns)
-})
-.catch(error => console.log(error));
-
-
+    showAlumnos(alumns);
+  })
+  .catch((error) => console.log(error));
 
 function showAlumnos(array) {
-    const table = document.getElementById("listStudents");
-    const modal = document.getElementById("containerModal")
-    let tableContent
+  const table = document.getElementById("listStudents");
+  const modal = document.getElementById("containerModal");
+  let tableContent;
 
-    array.forEach((item, index) => {
-        tableContent  +=  ` <tr>
-            <th scop="row" class="table-background">${item.id}</th>
+  array.forEach((item, index) => {
+    tableContent += ` <tr>
+            <td scop="row" class="table-background">${item.id}</td>
             <td>${item.name}</td>
             <td>${item.email}</td>
             <td>${item.boleta}</td>
@@ -41,8 +39,8 @@ function showAlumnos(array) {
             </button></td>
          </tr>`;
 
-         modal.innerHTML += `<div class="modal fade" id='${item.id}' tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-         <div class="modal-dialog">
+    modal.innerHTML += `<div class="modal fade" id='${item.id}' tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog modal-dialog-centered">
            <div class="modal-content">
              <div class="modal-header">
                <h1 class="modal-title fs-5" id="exampleModalLabel">${item.name}</h1>
@@ -55,43 +53,134 @@ function showAlumnos(array) {
              </div>
              <div class="modal-footer">
                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-               <button type="button" class="btn btn-primary">Save changes</button>
              </div>
            </div>
          </div>
-       </div>`
-
-        }
-    );
-    table.innerHTML = tableContent;
-
+       </div>`;
+  });
+  table.innerHTML = tableContent;
 }
 
-function showStudents() {
-    document.getElementById("tableStuden").style.display = "block";
-    document.getElementById("container-student").style.display = "none";
-}
-
-function showForm(){
-    document.getElementById("tableStuden").style.display = "none";
-    document.getElementById("container-student").style.display = "block";
-}
-
-
-function eliminarAlumno(id){
-
-    fetch(`https://universidad-backend.onrender.com/borrarAlumno/${id}`, {
-        method: 'DELETE'
-    }).then(response => response.json())
-    .then(data => {
-        if (data.status) {
-            document.getElementById('listStudents').innerHTML = ''
-            showAlumnos(data.alumnosFilter)
-            alert("Alumno eliminado")
-        } else {
-            alert("Alumno no eliminado")    
-        }
+function eliminarAlumno(id) {
+  fetch(`https://universidad-backend.onrender.com/borrarAlumno/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status) {
+        document.getElementById("listStudents").innerHTML = "";
+        showAlumnos(data.alumnosFilter);
+        alert("Alumno eliminado");
+      } else {
+        alert("Alumno no eliminado");
+      }
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
+}
 
+fetch("https://universidad-backend.onrender.com/obtenerProfesor")
+  .then((response) => response.json())
+  .then((data) => {
+    profesors = data.profesores;
+    console.log(profesors);
+    showProfesores(profesors);
+  })
+  .catch((error) => console.log(error));
+
+function showProfesores(array) {
+  const table = document.getElementById("listProfesors");
+  const modal = document.getElementById("containerModalProfesor");
+  let tableContent;
+
+  array.forEach((item, index) => {
+    tableContent += ` <tr>
+    <td scop="row" class="table-background">${item.id}</td>
+    <td>${item.nombre}</td>
+    <td>${item.campus}</td>
+    <td>${item.facultad}</td>
+    <td><button type="button" class="btn btn-danger" onclick="eliminarProfesor('${item.id}')">Eliminar Profesor</button></td>
+    <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${item.id}">Detalle del Profesor
+    </button></td>
+ </tr>`;
+
+    modal.innerHTML += `<div class="modal fade" id='${item.id}' tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">${item.nombre}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <p>Campus: ${item.campus}</p>
+          <p>Facultad: ${item.facultad}</p>
+          <p>Materia: ${item.materia}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  });
+  table.innerHTML = tableContent;
+}
+
+function eliminarProfesor(id) {
+  fetch(`https://universidad-backend.onrender.com/borrarProfesor/${id}`,{
+    method: "DELETE",
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.status) {
+      document.getElementById("listProfesors").innerHTML = "";
+      showProfesores(data.profesoresFilter);
+      alert("Profesor eliminado");
+    } else {
+      alert("Profesor eliminado");
+    }
+  })
+  .catch((error) => console.log(error));
+}
+
+function loginToStudent() {
+  const sectionStudent = document.getElementById("alumnos");
+  const sectionProfessors = document.getElementById("profesores");
+  const login = document.getElementById("login")
+
+  sectionProfessors.style.display = "none";
+  sectionStudent.style.display = "block"; // Opcional, si deseas mostrar la sección de estudiantes
+  login.style.display = "none";
+
+}
+function loginToProfesors() {
+  const sectionStudent = document.getElementById("alumnos");
+  const sectionProfessors = document.getElementById("profesores");
+  const login = document.getElementById("login")
+
+  login.style.display = "none"
+  sectionProfessors.style.display = "block"; 
+  sectionStudent.style.display = "none";
+}
+function loginStudent() {
+  const loginStudent = document.getElementById("loginAlumnos")
+  const loginProfesor = document.getElementById("loginProfesores")
+
+  loginStudent.style.display = "block"
+  loginProfesor.style.display = "none"
+}
+function loginProfesor() {
+  const loginStudent = document.getElementById("loginAlumnos")
+  const loginProfesor = document.getElementById("loginProfesores")
+
+  loginProfesor.style.display = "block"
+  loginStudent.style.display = "none"
+}
+function cerrarSesion() {
+  const sectionStudent = document.getElementById("alumnos");
+  const sectionProfessors = document.getElementById("profesores");
+  const loginStudent = document.getElementById("login")
+
+  loginStudent.style.display = "flex"
+  sectionProfessors.style.display = "none"; 
+  sectionStudent.style.display = "none";
 }
